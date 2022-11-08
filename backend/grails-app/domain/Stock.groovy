da6a829aed53284be992f0a74dd5f6a5a94f0a1b
Wilson Nunes <wilson-avn1@hotmail.com>
2022-11-07 22:40:14 -0300

    Teste Prático para Vaga de Desenvolvedor

---
+++ backend/grails-app/domain/Stock.groovy
@@ -0,0 +1,33 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter;
+
+class Stock {
+
+    double price;
+    LocalDateTime date;
+    Company company;
+
+    Stock(double price, LocalDateTime date, Company company) {
+        this.price = price;
+        this.date = date;
+        this.company = company;
+    }
+
+    public double getPrice() {
+        return price
+    }
+
+    public LocalDateTime getDate() {
+        return date
+    }
+
+    public Company getCompany() {
+        return company
+    }
+
+    public String quote() {
+        return company.name + '[' +
+            "price=" + price +
+            ", date=" + date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) + ']';
+    }
+}
